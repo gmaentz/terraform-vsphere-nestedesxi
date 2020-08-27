3aa206b35d40e26a853620f6f16b3ccdea4e542a
Gabe Maentz <gabe@maentz.net>
2020-08-27 09:00:18 -0400

    update TerraTests

--- test/vmware_test.go
+++ test/vmware_test.go
@@ -14,7 +14,19 @@ func TestTerraformExample(t *testing.T) {
 		TerraformDir: "../",
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
-			"var": "value",
+			"vcenter_user": "administrator@vsphere.local",
+			"vcenter_password":"RPTpass123!",
+			"vcenter_server": "192.168.169.35",
+			"esxi_root_password": "Password!23",
+			"datacenter_name": "Datacenter",
+			"datastore_name": "raid10_gen7",
+			"resource_pool": "192.168.169.34/Resources",
+			"network_name":"VM Network",
+			"source_esxi_host": "192.168.169.34",
+			"num_esxi_hosts": "1",
+			"nameprefix": "esxi67t",
+			"offset": 1,
+			"vm_template_name": "esxi67u3_template",
 		},
 
 		// Environment variables to set when running Terraform
